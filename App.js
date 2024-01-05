36fabe8fe96d1a34decd3d995380c4f9358250fb
sharmakrishna123 <sharma215krishna@gmail.com>
2024-01-05 21:01:21 +0530

    feat: adds index.html

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-/**
- * <div id="parent">
- *    <div id="chid1">
- *     <h1> I'm h1 tag</h1>
- *     <h2> I'm h2 tag</h2> 
- *   </div>
- *  <div id="chid2">
- *   <h1> I'm h1 tag</h1>
- *   <h2> I'm h2 tag</h2> 
- *  </div>
- *
- *
- *
- *
- *
- */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const parent = React.createElement("div",{id:"parent"},[
 React.createElement("div",{id:"chiid1"},[
-React.createElement("h1",{},"I'm h1 tag"),
+React.createElement("h1",{},"hello duniya wale "),
 React.createElement("h2",{},"I'm h2 tag")
 ]),
 React.createElement("div",{id:"chiid2"},[
